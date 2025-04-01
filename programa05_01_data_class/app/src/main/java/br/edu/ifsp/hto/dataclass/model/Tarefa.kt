3ac71f7f70a3518cb1c107745bcfdf5042cc9a2e
gubartz <gubartz@gmail.com>
2025-04-01 11:27:22 -0300

    Uso de Data Class

---
+++ programa05_01_data_class/app/src/main/java/br/edu/ifsp/hto/dataclass/model/Tarefa.kt
@@ -0,0 +1,30 @@
+package br.edu.ifsp.hto.dataclass.model
+
+data class Tarefa(
+    val id: Long,
+    val titulo: String,
+    val descricao: String?,
+    val ehFinalizada: Boolean
+)
+
+// fake dados
+val tarefa1 = Tarefa(
+    1,
+    "Tarefa 1",
+    descricao = "Descrição da Tarefa 1",
+    ehFinalizada = true
+)
+
+val tarefa2 = Tarefa(
+    2,
+    "Tarefa 2",
+    descricao = null,
+    ehFinalizada = false
+)
+
+val tarefa3 = Tarefa(
+    3,
+    "Tarefa 3",
+    descricao = "Descrição da Tarefa 3",
+    ehFinalizada = true
+)
